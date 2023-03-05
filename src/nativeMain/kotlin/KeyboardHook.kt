83dcaae479e0e58a9b5f0c46f8efc0be08f092ff
jaka2005 <jakalogin1808@gmail.com>
2023-03-05 19:23:13 +0400

    debug and created message loo[

--- src/nativeMain/kotlin/KeyboardHook.kt
+++ src/nativeMain/kotlin/KeyboardHook.kt
@@ -1,24 +1,33 @@
+import kotlinx.cinterop.pointed
 import kotlinx.cinterop.staticCFunction
+import kotlinx.cinterop.toCPointer
 import platform.windows.*
 
 
-private val hook: HHOOK = setHook(WH_KEYBOARD_LL, staticCFunction(::eventHook))
+private var hook: HHOOK? = null
 
 private fun eventHook(nCode: Int, wParam: WPARAM, lParam: LPARAM): LRESULT {
     return if (!KeyboardHook.processHook(nCode, wParam, lParam))
-        nextHook(hook, nCode, wParam, lParam)
+        nextHook(hook!!, nCode, wParam, lParam)
     else 1
 }
 
 object KeyboardHook {
+    fun init() {
+        hook = setHook(WH_KEYBOARD_LL, staticCFunction(::eventHook))
+    }
+
     fun processHook(nCode: Int, wParam: WPARAM, lParam: LPARAM): Boolean {
         if (nCode < 0)
             return false
+
+        val info = lParam.toCPointer<KBDLLHOOKSTRUCT>()!!.pointed
+        println("getting vkCode - ${info.vkCode}")
 
         return false
     }
 
     fun dispose() {
-        removeHook(hook)
+        removeHook(hook!!)
     }
 }
--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,3 +1,16 @@
-fun main() {
+import kotlinx.cinterop.*
+import platform.windows.*
 
+
+fun main() {
+    KeyboardHook.init()
+
+    memScoped {
+        val msg = alloc<MSG>()
+        while (GetMessage!!(msg.ptr, null, 0u, 0u).toInt() > 0) {
+            TranslateMessage(msg.ptr)
+            DispatchMessage!!(msg.ptr)
+        }
+    }
+    KeyboardHook.dispose()
 }
