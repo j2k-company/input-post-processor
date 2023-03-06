99db9c307ac7cb21b33db44a3f9bd094e829f030
jaka2005 <jakalogin1808@gmail.com>
2023-03-06 22:46:57 +0400

    realised basic logic for process kb input

--- src/nativeMain/kotlin/InteropUtils.kt
+++ src/nativeMain/kotlin/InteropUtils.kt
@@ -1,3 +1,7 @@
+import platform.windows.GetKeyState
 import platform.windows.WINBOOL
 
 fun WINBOOL.toBoolean() = this == 1
+
+fun keyPressed(keyCode: Int) =
+    (GetKeyState(keyCode).toInt() and 0x8000) == 0x8000
--- src/nativeMain/kotlin/KeyboardHook.kt
+++ src/nativeMain/kotlin/KeyboardHook.kt
@@ -13,6 +13,9 @@ private fun eventHook(nCode: Int, wParam: WPARAM, lParam: LPARAM): LRESULT {
 }
 
 object KeyboardHook {
+    private val cache = mutableListOf<Char>()
+    private var keyInput = false
+
     fun init() {
         hook = setHook(WH_KEYBOARD_LL, staticCFunction(::eventHook))
     }
@@ -22,12 +25,57 @@ object KeyboardHook {
             return false
 
         val info = lParam.toCPointer<KBDLLHOOKSTRUCT>()!!.pointed
-        println("getting vkCode - ${info.vkCode}")
+        val keyCode = info.vkCode.toInt()
+
+        when (wParam.toInt()) {
+            WM_KEYDOWN, WM_SYSKEYDOWN -> {
+                if (keyCode == 0x35 && keyPressed(VK_SHIFT)) {
+                    if(keyInput) {
+                        replaceInput(cache.joinToString(""))
+                        cache.clear()
+                    }
+
+                    keyInput = !keyInput
+                    return false
+                }
+
+                if (keyInput && isPrintable(keyCode) && !modifierKeysPressed()) {
+                     cache.add(keyCode.toChar())
+                }
+            }
+
+            WM_KEYUP, WM_SYSKEYUP -> {}
+        }
 
         return false
     }
 
-    fun dispose() {
-        removeHook(hook!!)
+    fun modifierKeysPressed() =
+        keyPressed(VK_CONTROL) || keyPressed(VK_MENU) || keyPressed(VK_LWIN) || keyPressed(VK_RWIN)
+
+    fun isPrintable(keyCode: Int) = when (keyCode) {
+        in 0x30..0x39,
+        in 0x41..0x5A,
+        in VK_NUMPAD0..VK_DIVIDE,
+        in VK_OEM_1..VK_OEM_3,
+        in VK_OEM_4..VK_OEM_8,
+        VK_OEM_102 -> true
+
+        else -> false
     }
+
+    fun replaceInput(key: String) {
+        TODO("Non yet implemented")
+    }
+
+    fun dispose() {
+        if (hook != null) {
+            removeHook(hook!!)
+            hook = null
+        }
+    }
+}
+
+enum class KeyType {
+    PRINTABLE, MODIFIER, FUNCTION, CURSOR, TOGGLE, NUMPAD
 }
