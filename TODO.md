1395dc1cc3105df5c9e1c3d0c22d0669b3cd5033
jaka2005 <jakalogin1808@gmail.com>
2023-03-16 00:50:51 +0400

    rewrite build.gradle script

--- TODO.md
+++ TODO.md
@@ -4,4 +4,3 @@
 * Rewrite the input processing of replacement keys
 * Add pop-up notifications with a suggestion to add a value when the user has entered a non-existent key
 * Add hints for keys (if possible)
-* Rewrite gradle build script
--- build.gradle.kts
+++ build.gradle.kts
@@ -12,21 +12,19 @@ repositories {
 
 kotlin {
     val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+
+    if (!hostOs.startsWith("Windows")) {
+        throw GradleException("Host OS is not supported")
     }
 
-    nativeTarget.apply {
+    mingwX64("native").apply {
         binaries {
-            executable {
+            executable("input-post-processor", listOf(RELEASE)) {
                 entryPoint = "main"
             }
         }
     }
+
     sourceSets {
         val nativeMain by getting {
             dependencies {
